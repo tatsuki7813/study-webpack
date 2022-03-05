0bccb1e99b8b666caf26318a1663f5f43c7f9e6c
kobayashi.tatsuki <ttk7813@gmail.com>
2022-03-05 21:07:02 +0900

    create test files

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HelloWorld } from "./HelloWorld.jsx";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HelloWorld />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
