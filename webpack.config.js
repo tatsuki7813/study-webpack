699cb11b27f16b1e862d796901435b9b3ba0653a
kobayashi.tatsuki <ttk7813@gmail.com>
2022-03-08 23:13:06 +0900

    remove context propaty

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  context: path.join(__dirname, "src"),
-  entry: "./app.jsx",
+  entry: "./src/app.jsx",
   output: {
     path: path.join(__dirname, "dist"),
     filename: "./app.js",
